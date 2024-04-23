08f483cde7e1ec28ae80e55e4eeafa565b83defb
Makson Vinicio <maksonvinicio7@gmail.com>
2024-04-23 10:19:32 -0300

    :bricks: ci: changing yaml file

--- tests/api/routes/test_predict.py
+++ tests/api/routes/test_predict.py
@@ -8,10 +8,13 @@ from tests.utils.predict import create_random_data_to_predict
 base_url = "http://127.0.0.1:8000"
 
 
+data = create_random_data_to_predict()
+
 @pytest.mark.asyncio
 async def test_root():
+
     async with AsyncClient(app=app, base_url=base_url) as client:
         response = await client.post(
-            "/api/v1/predict", data=create_random_data_to_predict
+            "/api/v1/predict", data=data
         )
     assert response.status_code == 200
--- tests/utils/predict.py
+++ tests/utils/predict.py
@@ -1,20 +1,20 @@
 import random
 
-from ...src.app.schemas.ml_model import Input
+from src.app.schemas.ml_model import Input
 
 
 def create_random_data_to_predict():
     input_data = {
-        "sample_code_number": random.randint(63375, 13454352),
-        "clump_thickness": random.randint(1, 10),
-        "uniformity_of_cell_size": random.randint(1, 10),
-        "uniformity_of_cell_shape": random.randint(1, 10),
-        "marginal_adhesion": random.randint(1, 10),
-        "single_epithelial_cell_size": random.randint(1, 10),
-        "bare_nuclei": random.randint(1.0, 10.0),
-        "bland_chromatin": random.randint(1, 10),
-        "normal_nucleoli": random.randint(1, 10),
-        "mitoses": random.randint(1, 10),
+        "sample_code_number": random.randrange(63375, 13454352),
+        "clump_thickness": random.randrange(1, 10),
+        "uniformity_of_cell_size": random.randrange(1, 10),
+        "uniformity_of_cell_shape": random.randrange(1, 10),
+        "marginal_adhesion": random.randrange(1, 10),
+        "single_epithelial_cell_size": random.randrange(1, 10),
+        "bare_nuclei": random.randrange(1.0, 10.0),
+        "bland_chromatin": random.randrange(1, 10),
+        "normal_nucleoli": random.randrange(1, 10),
+        "mitoses": random.randrange(1, 10),
     }
 
     data = Input(**input_data)
