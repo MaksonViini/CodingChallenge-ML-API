ec278632999c48c1ac01c8fda306d13421e64515
Makson Vinicio <maksonvinicio7@gmail.com>
2024-04-23 10:40:53 -0300

    :bricks: ci: changing yaml file

--- tests/api/routes/test_predict.py
+++ tests/api/routes/test_predict.py
@@ -2,28 +2,18 @@ import pytest
 from httpx import AsyncClient
 
 from src.app.main import app
+import json
 
 from tests.utils.predict import create_random_data_to_predict
 
 base_url = "http://127.0.0.1:8000"
 
-input_data = {
-  "sample_code_number": 999999,
-  "clump_thickness": 5,
-  "uniformity_of_cell_size": 10,
-  "uniformity_of_cell_shape": 10,
-  "marginal_adhesion": 3,
-  "single_epithelial_cell_size": 7,
-  "bare_nuclei": 3,
-  "bland_chromatin": 8,
-  "normal_nucleoli": 10,
-  "mitoses": 2
-}
+
 
 @pytest.mark.asyncio
 async def test_predict():
     async with AsyncClient(app=app, base_url=base_url) as client:
         response = await client.post(
-            "/api/v1/predict", data=input_data
+            "/api/v1/predict", json=create_random_data_to_predict()
         )
     assert response.status_code == 200
